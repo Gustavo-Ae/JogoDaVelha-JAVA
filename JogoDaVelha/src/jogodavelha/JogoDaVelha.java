5e409ed15ef420ac1a4c5a210143f0ecda02ea1f
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-07-17 10:59:07 -0300

    Criando a ação do clique nos 9 botões e travando os botões para depois que receber o click não mudar.

--- JogoDaVelha/src/jogodavelha/JogoDaVelha.java
+++ JogoDaVelha/src/jogodavelha/JogoDaVelha.java
@@ -1,5 +1,6 @@
 package jogodavelha;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,6 +9,7 @@ import javax.swing.JFrame;
 public class JogoDaVelha extends JFrame {
     JButton[] botoes = new JButton[9];
     boolean X_ou_O;
+    boolean[] click = new boolean[9]; // vai ser utilizado para quando clicar em um botão , se ele já estiver ativado com "O" ou "X" ele não vai poder trocar.
     
     public JogoDaVelha(){
         setVisible(true);
@@ -23,19 +25,116 @@ public class JogoDaVelha extends JFrame {
                 botoes[contador] = new JButton();
                 add(botoes[contador]);
                 botoes[contador].setBounds((100*i) + 50,(100*j) + 50 ,95,95);
+                botoes[contador].setFont(new Font("Arial",Font.BOLD,30));
                 contador++;
             }
         }
-         
-        botoes[0].addActionListener(new java.awt.event.ActionListener() {
+        
+        for(int i = 0; i < 9;i++){
+            click[i] = false;
+        }
+        
+        botoes[0].addActionListener(new java.awt.event.ActionListener(){
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[0] == false){
+                  click[0] = true;
                   X_ou_O = mudar(botoes[0]);
-            }
+               }
+          }
         });
-       
+        
+        botoes[1].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[1] == false){
+                  click[1] = true;
+                  X_ou_O = mudar(botoes[1]);
+               }
+          }
+        });
+        
+        botoes[2].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[2] == false){
+                  click[2] = true;
+                  X_ou_O = mudar(botoes[2]);
+               }
+          }
+        });
+        
+        botoes[3].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[3] == false){
+                  click[3] = true;
+                  X_ou_O = mudar(botoes[3]);
+               }
+          }
+        });
+        
+        botoes[4].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[4] == false){
+                  click[4] = true;
+                  X_ou_O = mudar(botoes[4]);
+               }
+          }
+        });
+        
+        botoes[5].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[5] == false){
+                  click[5] = true;
+                  X_ou_O = mudar(botoes[5]);
+               }
+          }
+        });
+        
+        botoes[6].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[6] == false){
+                  click[6] = true;
+                  X_ou_O = mudar(botoes[6]);
+               }
+          }
+        });
+        
+        botoes[7].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[7] == false){
+                  click[7] = true;
+                  X_ou_O = mudar(botoes[7]);
+               }
+          }
+        });
+        
+        botoes[8].addActionListener(new java.awt.event.ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              if(click[8] == false){
+                  click[8] = true;
+                  X_ou_O = mudar(botoes[8]);
+               }
+          }
+        });
+        
     }
+    
     public boolean mudar(JButton btn){
         if(X_ou_O){
             btn.setText("O");
